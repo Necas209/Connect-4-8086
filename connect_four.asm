437a87056f8a1b1575103bf2fb32014c5a6782e8
al70633 <al70633@utad.eu>
2020-12-28 22:41:09 +0000

    Novo trabalho - Connect Four
    Face aos problemas envolvidos com a recursividade de comer várias peças, optei por experimentar programar Connect Four.

---
+++ connect_four.asm
@@ -0,0 +1,332 @@
+include emu8086.inc
+
+JMP start
+
+    linha1 DB 7 DUP(' ')
+    linha2 DB 7 DUP(' ')
+    linha3 DB 7 DUP(' ')
+    linha4 DB 7 DUP(' ')
+    linha5 DB 7 DUP(' ')
+    linha6 DB 7 DUP(' ')   
+    
+      
+    frame2 DB 204,205,206,205,206,205,206,205,206,205,206,205,206,205,185,"$"  ;lateral c/ lig    
+    frame3 DB 200,205,202,205,202,205,202,205,202,205,202,205,202,205,188,"$"  ;inferior    
+    frame4 DB 186," ",186," ",186," ",186," ",186," ",186," ",186," ",186,"$" ;lateral s/ lig 
+
+    turn DB " "
+    
+    sim DB 1
+    
+    msg_p1 DB "Jogador 1$"
+    msg_p2 DB "Jogador 2$" 
+    msg_jogada DB "Indique a coluna: $"
+    
+    p1_nome DB "Jogador 1 (",1,") -           $"
+    p2_nome DB "Jogador 2 (",2,") -           $"
+    msg_ask_p1 DB "Nome do jogador 1: $"
+    msg_ask_p2 DB "Nome do jogador 2: $"
+    
+    msg_erro DB "Erro.$"
+    
+DEFINE_CLEAR_SCREEN
+    
+                       
+start:
+    
+    MOV AX, 0B800h
+    MOV ES, AX
+    
+    CALL perguntar_nomes
+    
+    CALL CLEAR_SCREEN
+    
+    CALL imprimir_moldura
+    
+    CALL imprimir_nomes
+    
+    CALL RANDSTART  ;escolhe ordem dos jogadores
+    
+    LEA BX, turn    ;guarda a vez ("turn")
+    MOV [BX], AL
+    
+    CMP AL, 0       ;turn=0 => p1 seguido de p2
+    JE p1_p2
+    
+    CMP AL, 1       ;turn=1 => p2 seguido de p1
+    JE p2_p1
+     
+    p1_p2:
+    CALL imprimir_tabuleiro
+    
+    CALL jogada
+    
+    LEA BX, turn
+    MOV [BX], 1
+    
+    CALL imprimir_tabuleiro
+    
+    CALL jogada
+    
+    LEA BX, turn
+    MOV [BX], 0  
+   
+    CMP [sim], 1
+    
+    LOOPE p1_p2
+    
+    JMP FIM
+    
+    p2_p1:
+    
+    CALL imprimir_tabuleiro
+    
+    CALL jogada
+    
+    LEA BX, turn
+    MOV [BX], 1
+    
+    CALL imprimir_tabuleiro
+    
+    CALL jogada
+    
+    LEA BX, turn
+    MOV [BX], 0  
+   
+    CMP [sim], 1
+    
+    LOOPE p1_p2
+    
+    FIM:
+    
+    MOV AX, 4C00h
+    INT 21h
+
+;////////////////////////////////// 
+
+
+RANDSTART:          ; gera numero aleatorio entre 0 e 1
+   MOV AH, 00h       
+   INT 1AH     
+
+   MOV  AX, DX
+   XOR  DX, DX
+   MOV  CX, 2    
+   DIV  CX
+   MOV  AH, 0 
+   MOV  AL, DH   
+RET
+
+
+jogada PROC
+    
+    jog:
+    GOTOXY 20, 2
+    
+    CMP turn[0], 0
+    JE jogada_msg_p1
+    JMP jogada_msg_p2
+    
+    jogada_msg_p1:
+    MOV AH, 9
+    LEA DX, msg_p1
+    INT 21h
+    JMP jogada_aux
+    
+    jogada_msg_p2:
+    MOV AH, 9
+    LEA DX, msg_p2
+    INT 21h
+    
+    jogada_aux:    
+    GOTOXY 20, 4
+    
+    MOV AH, 9
+    LEA DX, msg_jogada
+    INT 21h
+    
+    MOV AH, 1
+    INT 21h
+    
+    CMP AL, "1"
+    JL jog_erro
+    CMP AL, "7"
+    JG jog_erro
+    
+    SUB AL, "1"
+    MOV AH, 0
+    MOV BX, AX      ; offset dentro da linha, ou seja, a coluna
+    XOR SI, SI
+    
+    loop_jog:
+    CMP SI, 42
+    JE jog_erro
+    SUB SI, 7  
+    CMP [linha6+BX+SI+7], ' '
+    JNE loop_jog
+    
+    LEA DI, [linha6+BX+SI+7]
+    
+    CMP turn[0], 0
+    JE jogada_p1
+    JMP jogada_p2
+    
+    jogada_p1:
+    MOV [DI], 1
+    RET
+    
+    jogada_p2:
+    MOV [DI], 2
+    RET
+    
+    jog_erro:
+    CALL erro
+    JMP jog                                                      
+     
+jogada ENDP                                                      
+
+
+erro:
+
+    GOTOXY 20, 4
+    
+    MOV AH, 9
+    LEA DX, msg_erro
+    INT 21h
+    
+    RET
+
+
+perguntar_nomes PROC
+
+    LEA DX, msg_ask_p1
+    GOTOXY 2, 2
+    MOV AH, 9
+    INT 21h
+    
+    LEA BX, p1_nome
+    ADD BX, 16
+    CALL ler_nome
+    
+    LEA DX, msg_ask_p2
+    GOTOXY 2, 4
+    MOV AH, 9
+    INT 21h
+    
+    LEA BX, p2_nome
+    ADD BX, 16
+    CALL ler_nome
+    
+    RET
+
+perguntar_nomes ENDP
+
+ler_nome:
+    
+    MOV DI, 0
+    MOV CX, 10 
+    
+    ler_char:
+    
+       MOV AH, 1
+       INT 21h
+       
+       MOV [BX+DI], AL
+       
+       INC DI
+       
+       CMP AL, 13
+       
+    LOOPNE ler_char 
+    
+    DEC DI
+    
+    MOV [BX+DI], ' '
+    
+    RET
+
+
+imprimir_nomes:
+    
+    GOTOXY 2, 14
+    LEA DX, p1_nome
+    MOV AH, 9
+    INT 21h
+    
+    GOTOXY 2, 16
+    LEA DX, p2_nome
+    MOV AH, 9
+    INT 21h
+    
+    RET
+    
+
+imprimir_moldura:
+    
+    MOV BL, 1
+    
+    MOV CX, 5
+    c1: 
+        GOTOXY 0, BL
+        
+        LEA DX, frame4
+        MOV AH, 9
+        INT 21h
+        
+        INC BL
+        
+        GOTOXY 0, BL
+        
+        LEA DX, frame2
+        MOV AH, 9
+        INT 21h
+        
+        INC BL
+    LOOP c1
+    
+    GOTOXY 0, BL
+    
+    LEA DX, frame4
+    MOV AH, 9
+    INT 21h
+    
+    INC BL
+    
+    GOTOXY 0, BL
+    
+    LEA DX, frame3
+    MOV AH, 9
+    INT 21h
+    
+    RET
+ 
+              
+imprimir_tabuleiro PROC
+
+    MOV BP, 0
+    MOV SI, 0
+    MOV CX, 6
+    c:
+        PUSH CX
+        MOV CX, 7
+        c2: 
+            LEA BX, linha1
+            MOV AL, ES:[0A0h+BP]
+            MOV AH, [BX+SI]
+            CMP AL, AH
+            JE avancar_imp
+            MOV ES:[0A0h+BP], AH
+            avancar_imp:
+            ADD BP, 4
+            INC SI 
+        LOOP c2
+        POP CX
+        ADD BP, 120h 
+    LOOP c
+    
+    RET
+     
+imprimir_tabuleiro ENDP
+ 
+
+END
--- damas.asm
+++ damas.asm
@@ -20,21 +20,25 @@ JMP start
     frame11 DB 7 DUP (' ', 186)
     frame12 DB " ", 186,"$"
     
-    L1 DB 4 DUP (4, 219)
-    L2 DB 4 DUP (219, 4)
-    L3 DB 4 DUP (4, 219)
-    L4 DB 4 DUP (219, 0)
-    L5 DB 4 DUP (0, 219)
-    L6 DB 4 DUP (219, 6)
-    L7 DB 4 DUP (6, 219)
-    L8 DB 4 DUP (219, 6)
+    L1 DB 4 DUP (219, 4)
+    L2 DB 4 DUP (4, 219)
+    L3 DB 4 DUP (219, 4)
+    L4 DB 4 DUP (0, 219)
+    L5 DB 4 DUP (219, 0)
+    L6 DB 4 DUP (6, 219)
+    L7 DB 4 DUP (219, 6)
+    L8 DB 4 DUP (6, 219)
 
     nome DB 10 DUP(32)
     
     perguntar_nome DB "Introduza o nome: $"
     
     pecas DB 12,12
+    
+    fim DB 0
 
+    turn DB " "
+    
     mem DW 01E0h, 31Ch, 0458h, 0594h, 06D0h, 080Ch, 0948h, 0A84h
 
 start:
@@ -50,14 +54,122 @@ start:
     MOV ES, AX       ; segmento extra para escrever no ecra
     
     CALL ask_name
-    
-    CALL CLEAR_SCREEN
 
     CALL print_tab
-
+    
+    CALL RANDSTART
+    
+    CMP [turn], 0
+    JE p1_p2
+    JMP p2_p1
+    
+    p1_p2:
+    loop1:
+    CALL p_turn
+    INC turn[0]
+    CALL p_turn
+    DEC turn[0]
+    CMP  [fim], 0
+    LOOPNE loop1
+    JMP ending
+    
+    p2_p1:
+    loop2:
+    CALL p_turn
+    DEC turn[0]
+    CALL p_turn
+    INC turn[1]
+    CMP  [fim], 0
+    LOOPNE loop2
+    
+    ending:
     MOV AX, 4C00h
     INT 21h
 
+msg_ask_move DB "Insira a posicao da peca que pretende mover:$"
+msg_erro DB "Peca/jogada invalida.$"
+msg_x DB "X: $"
+msg_y DB "Y: $"
+possible_moves DW 4 DUP(-1)
+
+p_turn PROC 
+    turn_restart:
+    GOTOXY 12, 2
+    LEA DX, msg_ask_move
+    MOV AH, 9
+    INT 21h
+    
+    GOTOXY 12, 4 
+    LEA DX, msg_x
+    MOV AH, 9
+    INT 21h
+    
+    GOTOXY 12, 6
+    MOV BH, DL
+    LEA DX, msg_y
+    MOV AH, 9
+    INT 21h
+    
+    MOV BL, DL
+    CALL convert_coord
+    
+    CMP turn[0], 0
+    JE p1
+    CMP turn[1], 1
+    JE p2
+    
+    p2:
+        CMP [L1+BX], 4
+        JE p2_peao
+        CMP [L1+BX], 3
+        JE p2_dama
+        JMP erro
+        
+    p1:
+        CMP [L1+BX], 6
+        JE p1_peao
+        CMP [L1+BX], 5
+        JE p1_dama
+        
+    erro:
+    GOTOXY 12, 8
+    MOV AH, 9
+    LEA DX, msg_erro
+    INT 21h
+    JMP turn_restart
+    
+    p1_peao:
+        CMP BX, 9
+        JNL     
+p_turn PROC
+    
+check_eatable PROC
+            
+check_eatable ENDP
+
+convert_coord PROC  ; recebe coordenadas (x,y) em BX e converte-as para um offset no tabuleiro
+    DEC BH
+    DEC BL
+    MOV AX, BX
+    MUL AL, 8
+    ADD AH, AL
+    XOR BX, BX
+    MOV BL, AH
+    RET
+convert_coord ENDP
+             
+p_turn ENDP    
+        
+RANDSTART:          ; gera numero aleatorio entre 0 e 1
+   MOV AH, 00h       
+   INT 1AH     
+
+   mov  ax, dx
+   xor  dx, dx
+   mov  cx, 2    
+   div  cx
+   MOV  [turn], DH   
+RET
 
 print_line MACRO addr
     PUSH AX
@@ -71,6 +183,8 @@ ENDM
 
    
 print_tab PROC
+    
+    CALL CLEAR_SCREEN
     
     GOTOXY 2, 2            ;moldura
     print_line frame1
