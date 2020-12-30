6bb893c5553fbdc45d1c346edc4c110027d56da2
al70633 <al70633@utad.eu>
2020-12-30 02:25:24 +0000

    Cores e verificação H/V
    - Tabuleiro com cores
    - Peças amarelas e vermelhas consoante o jogador
    - check_horizontal e check_vertical concluídos
    - várias novas funções e variáveis auxiliares
    - reestruturação do "start"

--- connect_four.asm
+++ connect_four.asm
@@ -2,6 +2,20 @@ include emu8086.inc
 
 JMP start
 
+MOV AX, 0b800h
+MOV DS, AX
+
+MOV CX, 16 
+MOV SI, 1
+MOV BL, 00h
+teste:
+MOV DS:[SI], BL
+ADD SI, 2
+ADD BL, 10h
+LOOP teste 
+
+RET
+
     linha1 DB 7 DUP(' ')    ;|      ; 1 -> pecas do p1
     linha2 DB 7 DUP(' ')    ;|      ; 2 -> pecas do p2
     linha3 DB 7 DUP(' ')    ;|
@@ -14,9 +28,7 @@ JMP start
     frame3 DB 200,205,202,205,202,205,202,205,202,205,202,205,202,205,188,"$"  ;inferior    
     frame4 DB 186," ",186," ",186," ",186," ",186," ",186," ",186," ",186,"$" ;lateral s/ lig 
 
-    turn DB " "
-    
-    sim DB 1
+    turn DB " "      ; vez do jogador: 0 -> p1, 1 -> p2
     
     msg_p1 DB "Jogador 1$"
     msg_p2 DB "Jogador 2$" 
@@ -27,25 +39,21 @@ JMP start
     msg_ask_p1 DB "Nome do jogador 1: $"
     msg_ask_p2 DB "Nome do jogador 2: $"
     
+    msg_fim_1 DB "O jogo terminou! O jogador 1 ganhou.$"
+    msg_fim_2 DB "O jogo terminou! O jogador 2 ganhou.$"
+    
     msg_erro DB "Erro.$"
     
-    last_position DW -1
+    last_position DB 2 DUP(-1)     ; posicao da ultima peca colocada
     
 DEFINE_CLEAR_SCREEN
-    
-check_horizontal PROC
-    
-    
-    RET
-    
-check_horizontal ENDP
 
 
 start:
     
     MOV AX, 0B800h
     MOV ES, AX
-    
+	
     CALL perguntar_nomes
     
     CALL CLEAR_SCREEN
@@ -65,56 +73,96 @@ start:
     CMP AL, 1       ;turn=1 => p2 seguido de p1
     JE p2_p1
      
-    p1_p2:
-    CALL imprimir_tabuleiro
-    
-    CALL jogada
-    
-    LEA BX, turn
-    MOV [BX], 1
-    
-    CALL imprimir_tabuleiro
-    
-    CALL jogada
-    
-    LEA BX, turn
-    MOV [BX], 0  
+    p1_p2:            
+        MOV CX, 3
+        loop_inicial12:
+            PUSH CX
+            
+            CALL jogada
    
-    CMP [sim], 1
+            LEA BX, turn
+            MOV [BX], 1
     
-    LOOPE p1_p2
+            CALL jogada
+ 
+            LEA BX, turn
+            MOV [BX], 0
+            
+            POP CX    
+        LOOP loop_inicial12
+        
+        loop_final12:
+            CALL jogada
+  
+            CALL check_horizontal
+        
+            CALL check_vertical
+            
+            CALL check_diagonal
+            
+            LEA BX, turn
+            MOV [BX], 1
     
-    JMP FIM
+            CALL jogada
     
-    p2_p1:
+            CALL check_horizontal
+        
+            CALL check_vertical
+            
+            CALL check_diagonal
+            
+            LEA BX, turn
+            MOV [BX], 0      
+        JMP loop_final12
     
-    CALL imprimir_tabuleiro
-    
-    CALL jogada
-    
-    LEA BX, turn
-    MOV [BX], 1
-    
-    CALL imprimir_tabuleiro
-    
-    CALL jogada
-    
-    LEA BX, turn
-    MOV [BX], 0  
+    p2_p1:            
+        MOV CX, 3
+        loop_inicial21:
+            PUSH CX
+            
+            CALL jogada
    
-    CMP [sim], 1
+            LEA BX, turn
+            MOV [BX], 0
     
-    LOOPE p1_p2
+            CALL jogada
+ 
+            LEA BX, turn
+            MOV [BX], 1
+            
+            POP CX    
+        LOOP loop_inicial21
+        
+        loop_final21:
+            CALL jogada
+  
+            CALL check_horizontal
+        
+            CALL check_vertical
+            
+            CALL check_diagonal
+            
+            LEA BX, turn
+            MOV [BX], 0
     
-    FIM:
+            CALL jogada
     
-    MOV AX, 4C00h
-    INT 21h
+            CALL check_horizontal
+        
+            CALL check_vertical
+            
+            CALL check_diagonal
+            
+            LEA BX, turn
+            MOV [BX], 1      
+        JMP loop_final21    
+    
 
 ;////////////////////////////////// 
 
 
 RANDSTART:          ; gera numero aleatorio entre 0 e 1
+   
    MOV AH, 00h       
    INT 1AH     
 
@@ -124,6 +172,7 @@ RANDSTART:          ; gera numero aleatorio entre 0 e 1
    DIV  CX
    MOV  AH, 0 
    MOV  AL, DH   
+
 RET
 
 
@@ -134,21 +183,23 @@ jogada PROC
     
     CMP turn[0], 0
     JE jogada_msg_p1
+    
     JMP jogada_msg_p2
     
     jogada_msg_p1:
-    MOV AH, 9
-    LEA DX, msg_p1
-    INT 21h
-    JMP jogada_aux
+        MOV AH, 9
+        LEA DX, msg_p1
+        INT 21h
+        
+        JMP jogada_aux
     
     jogada_msg_p2:
-    MOV AH, 9
-    LEA DX, msg_p2
-    INT 21h
+        MOV AH, 9
+        LEA DX, msg_p2
+        INT 21h
     
     jogada_aux:    
-    GOTOXY 20, 4
+        GOTOXY 20, 4
     
     MOV AH, 9
     LEA DX, msg_jogada
@@ -159,45 +210,256 @@ jogada PROC
     
     CMP AL, "1"
     JL jog_erro
+    
     CMP AL, "7"
     JG jog_erro
     
     SUB AL, "1"
     MOV AH, 0
     MOV BX, AX      ; offset dentro da linha, ou seja, a coluna
-    XOR SI, SI
+    
+    MOV SI, 0
     
     loop_jog:
-    CMP SI, 42
-    JE jog_erro
-    ADD SI, 7  
-    CMP [linha1+BX+SI-7], ' '
+        CMP SI, 42
+        JE jog_erro
+        
+        ADD SI, 7  
+        
+        CMP [linha1+BX+SI-7], ' '
     JNE loop_jog
     
     LEA DI, [linha1+BX+SI-7]
     
-    LEA BX, last_position
-    MOV [BX], DI
+    CALL coord
     
     CMP turn[0], 0
     JE jogada_p1
+    
     JMP jogada_p2
     
     jogada_p1:
-    MOV [DI], 1
-    RET
+        MOV [DI], 1
+        
+        JMP fim_jogada
     
     jogada_p2:
-    MOV [DI], 2
-    RET
+        MOV [DI], 2
     
+    fim_jogada:
+        CALL atualizar_tabuleiro
+        
+        RET
+        
     jog_erro:
-    CALL erro
-    JMP jog                                                      
+        CALL erro
+        
+        JMP jog                                                      
      
 jogada ENDP                                                      
 
 
+check_diagonal PROC
+    
+    MOV AL, turn[0]
+    INC AL
+    
+    MOV SI, 0
+    MOV CX, 7
+    
+    diag_loop1:
+        CMP [linha4+SI], AL
+        JNE diag_si
+        
+        MOV AH, 1
+        JMP diag_cont
+        
+        diag_si:
+            INC SI
+    LOOP diag_loop1
+    
+    diag_cont:
+    
+    RET
+    
+check_diagonal ENDP
+
+
+check_vertical PROC
+    
+    CMP last_position[1], 3
+    JGE vertical
+    
+    RET 
+    
+    vertical:
+        MOV AH, 0                ;AH = contador de pecas
+        MOV AL, turn[0]    
+        ADD AL, 1                ;AL = peca
+        CALL linha_horizontal    ;BX=linha
+        
+        XOR CX, CX
+        MOV CL, last_position[0] 
+        ADD BX, CX ;BX=linha+offset(coluna)
+        
+        MOV SI, 0
+        MOV CX, 4
+        
+        v_loop:
+            CMP [BX+SI], AL
+            JE inc_v
+            MOV AH, 0
+            JMP v_loop_end
+            
+            inc_v:
+                INC AH
+                CMP AH, 4
+                JE fim_do_jogo_v
+            
+            v_loop_end:
+                SUB SI, 7                    
+        LOOP v_loop
+            
+    RET
+    
+    fim_do_jogo_v:
+        CALL FIM_DO_JOGO
+        
+    RET
+
+check_vertical ENDP
+
+
+check_horizontal PROC
+    
+    MOV AH, 0            ;AH = contador de pecas
+    MOV AL, turn[0]    
+    ADD AL, 1            ;AL = peca
+    
+    CALL linha_horizontal  ;BX = linha
+    
+    MOV CX, 7
+    MOV SI, 0            ;SI = coluna
+    
+    h_loop:
+        CMP [BX+SI], AL
+        JE inc_h
+        MOV AH, 0
+        JMP h_loop_terminar
+    
+        inc_h:
+            INC AH    
+            CMP AH, 4
+            JE fim_do_jogo_h    
+    
+        h_loop_terminar:
+            INC SI
+    LOOP h_loop
+   
+    RET
+    
+    fim_do_jogo_h:
+        CALL FIM_DO_JOGO
+        
+    RET
+    
+check_horizontal ENDP
+
+
+FIM_DO_JOGO PROC
+        
+    GOTOXY 20, 8
+    
+    CMP turn[0], 0
+    JE fim_1
+    
+    JMP fim_2
+    
+    fim_1:
+        LEA DX, msg_fim_1
+        MOV AH, 9
+        INT 21h
+    
+    JMP fim_g
+    
+    fim_2:
+        LEA DX, msg_fim_2
+        MOV AH, 9
+        INT 21h
+    
+    fim_g: 
+        MOV AX, 4C00h
+        INT 21h
+    
+    RET
+    
+FIM_DO_JOGO ENDP
+    
+
+linha_horizontal PROC
+    
+    CMP last_position[1], 0
+    JE h1
+    
+    CMP last_position[1], 1
+    JE h2 
+    
+    CMP last_position[1], 2
+    JE h3 
+    
+    CMP last_position[1], 3
+    JE h4
+    
+    CMP last_position[1], 4
+    JE h5
+    
+    JMP h6
+    
+    h1:
+        LEA BX, linha1
+        
+        RET    
+    h2:
+        LEA BX, linha2
+        
+        RET    
+    h3:
+        LEA BX, linha3
+        
+        RET    
+    h4:
+        LEA BX, linha4
+        
+        RET    
+    h5:
+        LEA BX, linha5
+        
+        RET    
+    h6:
+        LEA BX, linha6
+        
+        RET
+    
+linha_horizontal ENDP
+
+
+coord:
+
+    MOV AX, SI
+    
+    LEA BP, last_position
+    
+    MOV [BP], BL             ; x = BL (pois BH = 0)
+    
+    SUB AX, 7                ; AX = SI -7
+    MOV BL, 7               
+    DIV BL                   ; AX = n.o linha (AL, pois AH = 0)
+    
+    MOV [BP+1], AL           ; y = AL 
+    
+    RET
+    
+        
 erro:
 
     GOTOXY 20, 4
@@ -210,36 +472,40 @@ erro:
 
 
 perguntar_nomes PROC
-
-    LEA DX, msg_ask_p1
+    
     GOTOXY 2, 2
+    
     MOV AH, 9
+    LEA DX, msg_ask_p1
     INT 21h
     
     LEA BX, p1_nome
     ADD BX, 16
+    
     CALL ler_nome
     
-    LEA DX, msg_ask_p2
     GOTOXY 2, 4
+    
     MOV AH, 9
+    LEA DX, msg_ask_p2
     INT 21h
     
     LEA BX, p2_nome
     ADD BX, 16
+    
     CALL ler_nome
     
     RET
 
 perguntar_nomes ENDP
 
+
 ler_nome:
     
     MOV DI, 0
     MOV CX, 10 
     
-    ler_char:
-    
+    ler_char:        
        MOV AH, 1
        INT 21h
        
@@ -247,8 +513,7 @@ ler_nome:
        
        INC DI
        
-       CMP AL, 13
-       
+       CMP AL, 13       
     LOOPNE ler_char 
     
     DEC DI
@@ -261,11 +526,13 @@ ler_nome:
 imprimir_nomes:
     
     GOTOXY 2, 14
+    
     LEA DX, p1_nome
     MOV AH, 9
     INT 21h
     
     GOTOXY 2, 16
+    
     LEA DX, p2_nome
     MOV AH, 9
     INT 21h
@@ -275,10 +542,10 @@ imprimir_nomes:
 
 imprimir_moldura:
     
-    MOV BL, 1
-    
+    MOV BL, 1    
     MOV CX, 5
-    c1: 
+    
+    c1:         
         GOTOXY 0, BL
         
         LEA DX, frame4
@@ -293,7 +560,7 @@ imprimir_moldura:
         MOV AH, 9
         INT 21h
         
-        INC BL
+        INC BL        
     LOOP c1
     
     GOTOXY 0, BL
@@ -310,38 +577,67 @@ imprimir_moldura:
     MOV AH, 9
     INT 21h
     
-    RET
- 
-              
-imprimir_tabuleiro PROC
-
-    MOV BP, 0
-    MOV SI, 35
-    MOV CX, 6
-    c:  
-        PUSH SI
+    MOV SI, 0    
+    MOV CX, 13
+    
+    m1: 
         PUSH CX
-        MOV CX, 7
-        c2: 
-            LEA BX, linha1
-            MOV AL, ES:[0A0h+BP]
-            MOV AH, [BX+SI]
-            CMP AL, AH
-            JE avancar_imp
-            MOV ES:[0A0h+BP], AH
-            avancar_imp:
-            ADD BP, 4
-            INC SI 
-        LOOP c2
-        POP CX
+        PUSH SI
+        
+        MOV CX, 15              
+        
+        m2: 
+            MOV ES:[9Fh+SI], 0000_1001b   ; mudar o tab. para azul
+            
+            ADD SI, 2    
+        LOOP m2
+        
         POP SI
-        SUB SI, 7
-        ADD BP, 120h 
-    LOOP c
+        POP CX 
+        
+        ADD SI, 9Eh         
+    LOOP m1    
     
     RET
-     
-imprimir_tabuleiro ENDP
  
+atualizar_tabuleiro PROC
+    
+    MOV AL, last_position[0]    
+    MOV BL, 2
+    MUL BL    
+    ADD AL, 1
+    MOV CL, AL
+    
+    MOV AL, last_position[1]
+    MOV BL, 2
+    MUL BL
+    MOV BL, 11
+    SUB BL, AL
+    
+    GOTOXY CL, BL
+    
+    
+    MOV AL, turn[0]
+    INC AL
+    
+    CMP AL, 1
+    JE amarelo
+    
+    MOV BL, 0000_1100b ; 1100b = vermelho claro
+    JMP update_cont
+    
+    amarelo:
+    MOV BL, 0000_1110b ; 1110b = amarelo
+    
+    update_cont:
+    
+    MOV AH, 09h
+    MOV BH, 0
+    MOV CX, 1 
+    INT 10h
+    
+    RET
+    
+atualizar_tabuleiro ENDP
 
 END
